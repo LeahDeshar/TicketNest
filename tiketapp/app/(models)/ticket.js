e79ea2654f93650783508e2d77701d11681f0e3b
Leah Deshar <leah.desar@gmail.com>
2024-05-15 22:22:43 +0545

    mongoose models

---
+++ tiketapp/app/(models)/ticket.js
@@ -0,0 +1,21 @@
+const { default: mongoose } = require("mongoose");
+
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.Promise = global.Promise;
+
+const ticketSchema = new Schema(
+  {
+    title: String,
+    description: String,
+    status: String,
+    category: String,
+    priority: Number,
+    progress: Number,
+    active: Boolean,
+  },
+  {
+    timestamps: true,
+  }
+);
+const Ticket = mongoose.models.Ticket || mongoose.model("Ticket", ticketSchema);
+export default Ticket;
