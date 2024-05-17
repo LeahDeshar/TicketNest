73c6289341d0985db1612a481efc1e95703513b3
Leah Deshar <leah.desar@gmail.com>
2024-05-17 11:04:44 +0545

    DeleteBlock

--- tiketapp/app/(components)/DeleteBlock.jsx
+++ tiketapp/app/(components)/DeleteBlock.jsx
@@ -1,11 +1,29 @@
+"use client";
+import { useRouter } from "next/navigation";
+
 const { faX } = require("@fortawesome/free-solid-svg-icons");
 const { FontAwesomeIcon } = require("@fortawesome/react-fontawesome");
 
-function DeleteBlock() {
+function DeleteBlock({ id }) {
+  const router = useRouter();
+  const deleteTicket = async () => {
+    try {
+      const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
+        method: "DELETE",
+      });
+      if (res.ok) {
+        router.refresh();
+      }
+      // router.reload();
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <FontAwesomeIcon
       icon={faX}
       className="text-red-400 hover:cursor-pointer hover:text-red-200"
+      onClick={deleteTicket}
     />
   );
 }
