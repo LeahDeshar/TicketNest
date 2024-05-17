1fd81f6c09ffa1a075721c0925bdacb169582c4f
Leah Deshar <leah.desar@gmail.com>
2024-05-17 10:26:39 +0545

    fetch and display

--- tiketapp/app/(components)/TicketCard.jsx
+++ tiketapp/app/(components)/TicketCard.jsx
@@ -7,14 +7,14 @@ const TicketCard = ({ ticket }) => {
   return (
     <div className="flex flex-col bg-card hover:bg-card-hover rounded-md shadow-lg p-3 m-2">
       <div className="flex mb-3">
-        <PriorityDisplay />
+        <PriorityDisplay priority={ticket.priority} />
         <div className="ml-auto">
           <DeleteBlock />
         </div>
       </div>
-      <h4>Ticket Title</h4>
+      <h4>{ticket.title}</h4>
       <hr className=" h-px border-0 bg-page mb-2" />
-      <p className=" whitespace-pre-wrap">{ticket.title}</p>
+      <p className=" whitespace-pre-wrap">{ticket.description}</p>
       <div className="flex-grow"></div>
       <div className="flex mt-2">
         <div className="flex flex-col">
